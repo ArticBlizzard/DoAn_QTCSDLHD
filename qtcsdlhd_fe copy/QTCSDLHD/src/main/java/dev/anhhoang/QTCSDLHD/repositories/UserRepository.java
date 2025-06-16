84749815dc3ea5969c9a78ff23369e55dada3eb3
AnhHoang0710 <99569650+ArticBlizzard@users.noreply.github.com>
2025-06-16 13:48:41 +0700

    Add files via upload

---
+++ qtcsdlhd_fe copy/QTCSDLHD/src/main/java/dev/anhhoang/QTCSDLHD/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+// src/main/java/dev/anhhoang/QTCSDLHD/repositories/UserRepository.java
+package dev.anhhoang.QTCSDLHD.repositories;
+
+import dev.anhhoang.QTCSDLHD.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    // We need a way to find a user by their email for login and to check if they already exist
+    Optional<User> findByEmail(String email);
+}
